0539fef0d0e8806c6f5a5934883e92225d7de367
Jason <leongjason3781@gmail.com>
2022-09-06 00:28:40 +0800

    m

--- Program.cs
+++ Program.cs
@@ -325,7 +325,7 @@ namespace ConsoleApp1
             //  Questions -------------------------------------------------------------------------------------------------------------------------------------------------------------------
             // lambda, deployment, indexers, await and sync, static and debugging, nameof expressions?, generics?, date and time, structs, delegates, reflection, garbage collector, appending to arrays all other data types. 
             // typeof, LINQ jason leong
-            Console.WriteLine(2);
+            Console.WriteLine(3);
 
 
         }
